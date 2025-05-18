75f0b66337557e1b824ba509a128a2104dbc8495
GregoriSimei <gregorisimei@gmail.com>
2025-05-18 17:16:42 -0300

    fix: adjust CIDR reference

--- postgres.tf
+++ postgres.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "postgres_sg" {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = ["0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     tags = {
