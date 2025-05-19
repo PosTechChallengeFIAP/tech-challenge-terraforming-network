78490900f1b64bc99a82441240e4cbe9d7dfa9b9
GregoriSimei <gregorisimei@gmail.com>
2025-05-18 21:16:37 -0300

    ci: add subnet group name to output

--- postgres-out.tf
+++ postgres-out.tf
@@ -2,3 +2,8 @@ output "postgres_sg_id" {
   description = "ID do security group do Postgres"
   value       = aws_security_group.postgres_sg.id
 }
+
+output "postgres_subnet_group_name" {
+  description = "Postgres subnet group name"
+  value = aws_db_subnet_group.postgres_subnet_group.name
+}
